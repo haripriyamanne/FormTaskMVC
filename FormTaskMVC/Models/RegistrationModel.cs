5c061714d3a0d74c355549c95cc74ebe8af4ecd3
haripriyamanne <haripriyamanne4@gmail.com>
2021-06-02 18:03:03 +0530

    Add project files.

---
+++ FormTaskMVC/Models/RegistrationModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormTaskMVC.Models
+{
+    public class RegistrationModel
+    {
+        [Required(ErrorMessage = "Name is mandatory")]
+        public string FirstName { get; set; }
+        
+        [Required(ErrorMessage = "Name is mandatory")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is mandatory")]
+        public string Email { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string Gender { get; set; }
+
+        public string CompanyName { get; set; }
+    }
+}
