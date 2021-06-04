3119b699771a14446f5b531116bf9ee7138918fe
haripriyamanne <haripriyamanne4@gmail.com>
2021-06-05 00:06:25 +0530

    Added Checkboxes in UI(Registration Form Page)

--- FormTaskMVC/Controllers/RegistrationController.cs
+++ FormTaskMVC/Controllers/RegistrationController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FormTaskMVC.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,47 @@ namespace FormTaskMVC.Controllers
     {
         public IActionResult RegistrationForm()
         {
-            return View();
+            List<CheckBoxModel> ChkItem = new List<CheckBoxModel>()
+        {
+          new CheckBoxModel {Value=1,Text="Software",IsChecked=false },
+          new CheckBoxModel {Value=1,Text="UI Developer",IsChecked=false },
+          new CheckBoxModel {Value=1,Text="BackEnd Developer",IsChecked=false },
+          new CheckBoxModel {Value=1,Text="Admin" ,IsChecked=false},
+          new CheckBoxModel {Value=1,Text="HR Services",IsChecked=false },
+          new CheckBoxModel {Value=1,Text="Full Stack Services" ,IsChecked=false},
+        };
+            //assigning records to the CheckBoxItems list     
+            RegistrationModel ChkItems = new RegistrationModel();
+            ChkItems.ServiceAreas = ChkItem;
+            return View(ChkItems);
         }
+        public IActionResult SaveRegistrations()
+        {
+            var model = new RegistrationModel()
+            {
+                FirstName = Request.Form["FirstName"],
+                LastName = Request.Form["LastName"],
+                Email = Request.Form["Email"],
+                CompanyName = Request.Form["CompanyName"],
+                CompanyWebsite = Request.Form["CompanyWebsite"],
+                Busineess=Request.Form["Business"],
+                Benifits = Request.Form["Benifits"],
+                ListProducts=Request.Form["ListProducts"],
+                TrustElements=Request.Form["TrustElements"],
+                ImportantAreas=Request.Form["ImportantAreas"],
+                WebsiteExample=Request.Form["WebsiteExample"]
+
+        //Gender = Request.
+        //PhoneNumber= Convert.ToInt32(Request.Form["PhoneNumber"])
+    };  
+            return View(model);
+        }
+
+        //public string SaveRegistrations(RegistrationModel registration)
+        //{
+        //    string fname = registration.FirstName;
+        //    return fname;
+        //}
+       
     }
 }
--- FormTaskMVC/Models/RegistrationModel.cs
+++ FormTaskMVC/Models/RegistrationModel.cs
@@ -8,19 +8,77 @@ namespace FormTaskMVC.Models
 {
     public class RegistrationModel
     {
-        [Required(ErrorMessage = "Name is mandatory")]
+        [Required(ErrorMessage = "FirstName is mandatory")]
+        [Display(Name="First Name")]
         public string FirstName { get; set; }
-        
-        [Required(ErrorMessage = "Name is mandatory")]
+        [Display(Name ="Last Name")]
+        [Required(ErrorMessage = "LastName is mandatory")]
         public string LastName { get; set; }
-
+        //[RegularExpression("^[a-z0-9][@][g][m][a][i][l][.][c][o][m]$")]
         [Required(ErrorMessage = "Email is mandatory")]
         public string Email { get; set; }
-
+        [Display(Name = "Phone Number")]
+        [RegularExpression("^[6-9]{1}[0-9]{9}$")]
+        [Required(ErrorMessage = "Phone Number is mandatory")]
         public int PhoneNumber { get; set; }
-
-        public string Gender { get; set; }
-
+        [Required(ErrorMessage = "Gender is mandatory")]
+        public Gender Gender { get; set; }
+        [Display(Name = "Company Name")]
+        [Required(ErrorMessage = "Company Name is mandatory")]
         public string CompanyName { get; set; }
+        [Display(Name = "Company Type")]
+        [Required(ErrorMessage = "Company Type is mandatory")]
+        public CompanyType CompanyType { get; set; }
+        [Display(Name = "Company Limited")]
+        [Required(ErrorMessage = "Company Limited is mandatory")]
+        public string CompanyLimited { get; set; }
+        [Display(Name = "Company Website")]
+        [Required(ErrorMessage = "Company Website is mandatory")]
+        public string CompanyWebsite { get; set; }
+        [Display(Name = "Please Describe Your Business")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        public string Busineess { get; set; }
+        [Display(Name = "Please Provide 3-7 Benefits that you offers to your Customers")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        public string Benifits { get; set; }
+        [Display(Name = "Please List the Products or Services you want to showcase your HomePage")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        public string ListProducts { get; set; }
+        [Display(Name = "Add 1-3 Trust Elements")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        [StringLength(100, MinimumLength = 3)]
+        public string TrustElements { get; set; }
+        [Display(Name = "List any Service areas you want to target")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        public List<CheckBoxModel> ServiceAreas { get; set; }
+        [Display(Name = "List Anything else you would like to mention or feel is important")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        public string ImportantAreas { get; set; }
+        [Display(Name = "List one website example that you like and what you like about them ?")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        public string WebsiteExample { get; set; }
+
     }
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public enum CompanyType
+    {
+        Manfacturing,
+        Industry,
+        Software,
+        NGO,
+        OutSourcing
+    }
+    //public enum ServiceAreas
+    //{
+    //    Cleaning,
+    //    Software,
+    //    UserInterface,
+    //    Product,
+    //    Backend
+    //}
+
 }
