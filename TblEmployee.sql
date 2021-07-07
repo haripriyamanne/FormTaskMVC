db6a610183a9b1124735056caf090354d7a9fafa
haripriyamanne <haripriyamanne4@gmail.com>
2021-07-07 22:08:25 +0530

    Added SQL Scripts

---
+++ TblEmployee.sql
@@ -0,0 +1,34 @@
+Create procedure spAddEmployee
+(    
+	@FirstName varchar(50),
+	@LastName varchar(50),
+	@Email varchar(50) ,
+	@PhoneNumber bigint,
+	@Gender varchar(50),
+	@CompanyName varchar(50),
+	@CompanyType varchar(50),
+	@CompanyLimited varchar(50),
+	@CompanyWebsite varchar(50),
+	@Busineess varchar(50),
+	@Benifits varchar(50),
+	@ListProducts varchar(50),
+	@TrustElements varchar(50),
+	@ServiceAreas varchar(50),
+	@ImportantAreas varchar(50),
+	@WebsiteExample varchar(50) 
+)    
+as     
+Begin     
+    Insert into TblEmployee (FirstName,LastName,Email,PhoneNumber,Gender,CompanyName,CompanyType,CompanyLimited,CompanyWebsite,Busineess,Benifits,ListProducts,
+	TrustElements,ServiceAreas,ImportantAreas,WebsiteExample ) 
+    Values (@FirstName,@LastName,@Email,@PhoneNumber,@Gender,@CompanyName,@CompanyType,@CompanyLimited,@CompanyWebsite,@Busineess,@Benifits,@ListProducts,
+	@TrustElements,@ServiceAreas,@ImportantAreas,@WebsiteExample )   
+End
+
+Create procedure spGetAllEmployees    
+as    
+Begin    
+    select *    
+    from TblEmployee 
+    order by Id    
+End
